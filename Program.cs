e81ec5619bd13f1c1c8605911c9e5ef6bedc3d0e
Sgajjala3s <139789854+Sgajjala3s@users.noreply.github.com>
2023-07-17 16:51:12 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Even and Odd Numbers program for first 100 numbers
+namespace EvenandOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even Numbers:");
+            for (int n = 0; n <= 100; n++)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.Write(n + " ");
+                }
+                
+            }
+            Console.WriteLine("\nodd Numbers:");
+            for (int n = 0; n <= 100; n++)
+            {
+                if(n%2 !=0)
+                {
+                    Console.Write(n + " ");
+                }
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
