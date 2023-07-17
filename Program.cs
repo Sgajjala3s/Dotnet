bcb41867898040912cd0ab82da30e5e521674cef
Sgajjala3s <139789854+Sgajjala3s@users.noreply.github.com>
2023-07-17 16:51:43 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,32 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Even and Odd Numbers program for first 100 numbers
-namespace EvenandOdd
+namespace Program1
 {
-    class Program
+    class Program  
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Entry Point for Console Application
         {
-            Console.WriteLine("Even Numbers:");
-            for (int n = 0; n <= 100; n++)
-            {
-                if (n % 2 == 0)
-                {
-                    Console.Write(n + " ");
-                }
-                
-            }
-            Console.WriteLine("\nodd Numbers:");
-            for (int n = 0; n <= 100; n++)
-            {
-                if(n%2 !=0)
-                {
-                    Console.Write(n + " ");
-                }
-            }
-            Console.WriteLine();
+            //creating instance of the employee class
+            Employees emp = new Employees();
+            emp.EmployeeName = "Sai Gajjala";
+
+            string result = emp.PrintEmployeeName(emp.EmployeeName);
+            Console.WriteLine("Result is: " + result);
             Console.ReadLine();
         }
     }
+    public class Employees
+    {
+        //constructor-Every class should have one Constructor.
+        public Employees() 
+        { 
+            Console.WriteLine("Constructor is called"); 
+        }    
+        public int Employeeid { get; set; }
+        public string EmployeeName { get; set; }
+        public string Gender { get; set; }
+        public string Location { get; set; }
+        public int Salary { get; set; }
+
+        public string PrintEmployeeName(string employeeName)//string in this line a return type for employeename.
+        {
+            Console.WriteLine("method is called");
+            string name = string.Empty;
+            if(employeeName !=null)
+            {
+                name = employeeName;
+            }
+            return name;
+        }
+    }
+    //Class -Blue print of a Type.
+    //method -Consits of public(public is a access modifier were any one can access)
 }
